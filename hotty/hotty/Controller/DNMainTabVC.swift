10b1e47a803d22c89179453df256a2c61df4a82e
FitzRoyLR <louis.regis18@gmail.com>
2018-03-28 19:54:07 +0200

    Changed the height of the tab bar

--- hotty/hotty/Controller/DNMainTabVC.swift
+++ hotty/hotty/Controller/DNMainTabVC.swift
@@ -1,5 +1,5 @@
 //
-//  UITabBarController.swift
+//  DNMainTabVC.swift
 //  hotty
 //
 //  Created by Louis Régis on 28/03/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UITabBarController: UIViewController {
+class DNMainTabVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +19,13 @@ class UITabBarController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func viewWillLayoutSubviews() {
+        var tabFrame = self.tabBar.frame
+        // - 40 is editable , the default value is 49 px, below lowers the tabbar and above increases the tab bar size
+        tabFrame.size.height = 70
+        tabFrame.origin.y = self.view.frame.size.height - 70
+        self.tabBar.frame = tabFrame
     }
     
 
