36aafb3ab8ea176ab04de76b1933c4966c1fa1c6
Louis Régis <louisregis@Louiss-MacBook-Pro.local>
2018-03-31 12:49:18 +0200

    lecture 20 done

--- hotty/hotty/Controller/PreviewCameraViewController.swift
+++ hotty/hotty/Controller/PreviewCameraViewController.swift
@@ -1,71 +1,72 @@
-////
-////  PreviewCameraViewController.swift
-////  hotty
-////
-////  Created by Louis Régis on 28/03/2018.
-////  Copyright © 2018 Louro. All rights reserved.
-////
 //
-//import UIKit
-//import FirebaseStorage
-//import FirebaseDatabase
+//  PreviewCameraViewController.swift
+//  hotty
 //
+//  Created by Louis Régis on 28/03/2018.
+//  Copyright © 2018 Louro. All rights reserved.
 //
-//class PreviewCameraViewController: UIViewController {
-//    
-//    @IBOutlet weak var photo: UIImageView!
-//    var image: UIImage!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        photo.image = self.image
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    @IBAction func cancelBtn_TouchUpInside(_ sender: Any) {
-//        dismiss(animated: true, completion: nil)
-//    }
-//    
-//    @IBAction func sendBtn_TouchUpInside(_ sender: Any) {
-//        if case self.image = self.image,  let imageData = UIImageJPEGRepresentation(photo, 0.1) {
-//            let photoIdString = NSUUID().uuidString
-//            print(photoIdString)
-//            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOT_REF).child("posts").child(photoIdString)
-//            storageRef.put(imageData, metadata: nil, completion: { (metadata, error) in
-//                if error != nil {
-//                ProgressHUD.showError(error!.localizedDescription)
-//                return
-//                }
-//                let photoUrl = metadata?.downloadURL()?.absoluteString
-//                self.sendDataToDatabase(photoUrl: photoUrl!)
-//            })
-//        } else {
-//            ProgressHUD.showError("Profile Image can't be empty")
-//        }
-//    }
-//    
-//    func sendDataToDatabase(photoUrl: String) {
-//        let ref = Database.database().reference()
-//        let postsReference = ref.child("posts")
-//        let newPostId = postsReference.childByAutoId().key
-//        let newPostReference = postsReference.child(newPostId)
-//        newPostReference.setValue(["photoUrl": photoUrl], withCompletionBlock: {
-//            (error, ref) in
-//            if error != nil {
-//                ProgressHUD.showError(error!.localizedDescription)
-//                return
-//            }
-//            ProgressHUD.showSuccess("Success")
-//        
-//    
-//    
-//    }
-//
-//
-//
-//)
-//}
-//
-//}
-//
+
+import UIKit
+import FirebaseStorage
+import FirebaseDatabase
+
+
+class PreviewCameraViewController: UIViewController {
+    
+    @IBOutlet weak var photo: UIImageView!
+    var image: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photo.image = self.image
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cancelBtn_TouchUpInside(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtn_TouchUpInside(_ sender: Any) {
+        let photo: UIImage = self.image
+        if self.image == self.image,  let imageData = UIImageJPEGRepresentation(photo, 0.1) {
+            let photoIdString = NSUUID().uuidString
+            print(photoIdString)
+            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOT_REF).child("posts").child(photoIdString)
+            storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    return
+                }
+                let photoUrl = metadata?.downloadURL()?.absoluteString
+                self.sendDataToDatabase(photoUrl: photoUrl!)
+            })
+        } else {
+            ProgressHUD.showError("Interest can't be empty")
+        }
+    }
+    
+    func sendDataToDatabase(photoUrl: String) {
+        let ref = Database.database().reference()
+        let postsReference = ref.child("posts")
+        let newPostId = postsReference.childByAutoId().key
+        let newPostReference = postsReference.child(newPostId)
+        newPostReference.setValue(["photoUrl": photoUrl], withCompletionBlock: {
+            (error, ref) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            ProgressHUD.showSuccess("Posted!")
+        
+    
+    
+    }
+
+
+
+)
+}
+
+}
+
+
